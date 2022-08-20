c21129f25b4869ad9b6b1b0a62f8f431a7ce2081
jayh0806 <107897337+jayh0806@users.noreply.github.com>
2022-08-20 13:51:00 -0400

    updated index.html and script.js

--- script.js
+++ script.js
@@ -1,209 +1,150 @@
-var quizBody = document.getElementById("quiz");
-var resultsel = document.getElementById("result");
-var finalScoreEl = document.getElementById("finalScore");
-var endgameDiv = document.getElementById("endgame");
-var questionsEl = document.getElementById("questions");
-var quizTime = document.getElementById("timer");
-var startQuizButton = document.getElementById("startbtn");
-var startQuizDiv = document.getElementById("startpage");
-var highscoreContainer = document.getElementById("highscoreContainer");
-var highscoreDiv = document.getElementById("high-scorePage");
-var highscoreInputName = document.getElementById("initials");
-var highscoreDisplayName = document.getElementById("highscore-initials");
-var endGameBtns = document.getElementById("endGameBtns");
-var submitScoreBtn = document.getElementById("submitScore");
-var highscoreDisplayScore = document.getElementById("highscore-score");
-var buttonA = document.getElementById("a");
-var buttonB = document.getElementById("b");
-var buttonC = document.getElementById("c");
-var buttonD = document.getElementById("d");
+// Jason Hardy assignment here
 
-var quizQuestions = [{
-    1: "Which company was behind the creation of Java?",
-    choiceA: "Yahoo",
-    choiceB: "Sun Microsystems",
-    choiceC: "Microsoft",
-    choiceD: "None of the above",
-    correctAnswer: "b"},
-    
-    {
-     2: "What is the name that all Java classes inherit (directly or indirectly) from?",
-     choiceA: "Object",
-     choiceB: "Class",
-     choiceC: "ClassLoader",
-     choiceD: "None of the above",
-     correctAnswer: "a"},
-     
-     {
-        3: "What is used to add styling to a web page?",
-        choiceA: "HTML",
-        choiceB: "CSS",
-        choiceC: "Javascript",
-        choiceD: "All the above",
-        correctAnswer: "b"},
+var generatePasswordBtn= document.querySelector("#generate");
 
-        {
-            4: "Which HTML attribute points to images,audio files, and video clips?",
-            choiceA: "href",
-            choiceB: "src",
-            choiceC: "class",
-            choiceD: "id",
-            correctAnswer: "b"},
+var passwordLength;
+var confirmLower;
+var confirmUpper;
+var confirmNumber;
+var confirmSpecial;
+var userChoices;
 
-            {
-                5: "The SCRIPT tag enables you to enhance a web page with code written in which programming language?",
-                choiceA: "SQL",
-                choiceB: "FORTRAN",
-                choiceC; "JavaScript",
-                choiceD; "Pascal",
-                correctAnswer: "c"},
-            {    
-                6: "How many NBA championships do the Boston Celtics have?",
-                choiceA: "20",
-                choiceB: "17",
-                choiceC: "5",
-                choiceD: "15",
-                correctAnswer: "b"},
+var lowerCase = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+// there must be an easier way
 
-                {
-                    7: "How many Super Bowl rings does Tom Brady have?",
-                    choiceA: "6",
-                    choiceB: "5",
-                    choiceC: "7",
-                    choiceD: "4",
-                    correctAnswer: "c"},
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*" , "." , ")" , "_", "-", "+", "=", "<" , ">" , "?" , "/", "~" , ];
 
-                    8: "Who was the first president of the United States of America?",
-                    choiceA: "George Washington",
-                    choiceB: "Abraham Lincoln",
-                    choiceC: "Bill Gates",
-                    ChoiceD: "Ronald Reagan",
-                    correctAnswer: "a"},
 
-                    9: "Is Die Hard a Christmas movie?",
-                    choiceA: "Yes",
-                    choiceB: "Absolutely",
-                    choiceC: "100%",
-                    choiceD: "All the Above",
-                    correctAnswer: "d"},
+// Get references to the #generate element
+var generatePassword= document.querySelector("#generate");
 
-                    10: "How many heading tags are in HTML?",
-                     choiceA: "Unlimited",
-                     choiceB: "6",
-                     choiceC: "1",
-                     choiceD: "10",
-                     correctAnswer: "b"},
-                
+// Write password to the #password input
+function writePassword() {
+  var password = generatePasswordBtn();
+  var passwordText = document.querySelector("#password");
 
-];
-// quiz questions found at https://www.funtrivia.com/trivia-quiz/SciTech/HTML-Fundamentals-407860.html and https://www.funtrivia.com/trivia-quiz/SciTech/Java-47826.html
-// some are for fun and to add more questions
-// more variables
-var finalQuestionIndex = quizQuestions.length;
-var currentQuestionIndex = 0;
-var timeLeft = 90;
-var timerInterval;
-var score = 0;
-var correct;
+  passwordText.value = password;
 
-function generateQuizQuestion(){
-    gameoverDiv.style.display = "none";
-    if (currentQuestionIndex === finalQuestionIndex){return showScore();}
 }
 
-var currentQuestion = quizQuestions[currentQuestionIndex];
-questionsEl.INNERHTML = "<p>" + currentQuestion.question + "<?p>";
-buttonA.innerHTML = currentQuestion.choiceA;
-buttonB.innerHTML = currentQuestion.choiceB;
-buttonC.innerHTML = currentQuestion.choiceC;
-buttonD.innerHTML = currentQuestion.choiceD;
-];
+// Add event listener to generate button
+generatePasswordBtn.addEventListener("click", writePassword);
 
-//Start Quiz function starts, timer and shows first question
-function startQuiz()[
-    endgameDiv.style.display = "none";
-    startQuizDiv.style.display = "none";
-    generateQuizQuestion();
+//Start Function
+function generatePassword() {
+  var passwordLength = prompt ("Choose password lenght between 8 and 128");
+  console.log("Password length" + passwordLength);
 
-    timerInterval = setInterval(function() {
-        timeLeft--;
-        quizTimer.textContent = "Time Left: " + timeLeft;
+  if(!passwordLength) {
+    alert("Required value");
 
-        if(timeLeft === 0) [
-            clearInterval(timerInterval);
-            showScore();
-        ]
+      } else if (passwordLength < 8 || password > 128) {
+        passwordLength = prompt("Password must be between 8 and 128");
+        console.log("Password length" + passwordLength);
 
-    }, 10000;
-    quizBody.style.display = "block";
-    }
-//displays score when finished 
-function showScore(){
-    quizBody.style.display = "none";
-    gameoverDiv.style.display = "flex";
-    highscoreInputName.value = "";
-    finalScoreEl.innerHTML = `You got${score}out of${quizQuestions.length}correct!`;
-    //above was suggested bu visual studio
-
-// click submit button highscore is saved and ranked
-
-    submitScoreBtn.addEventListener("click", function highscore(){
-        
-        if(highscoreInputName.value === "") {
-            alert("Name cannot be blank");
-            return false;
-        }else{
-            var savedHighscores = JSON.parse(localStorage.getItem("savedHighscores")) || [];
-            var currentUser = highscoreInputName.value.trim();
-            var currentHighscore = {name : currentUser, score: score 
-            };
-            
-            endgameDiv.style.display = " none ";
-            highscoreContainer.style.display = "flex";
-            highscoreDiv.style.display = "block";
-            endGameBtns.style.display = " flex " ;
-
-            savedHighscores.push(currentHighscore);
-            localStorage.setItem("savedHighscores",JSON.stringify(savedHighscores));
-            generateHighscores();
-
-        }
-
-    });
-
-    // allows repay of quiz by clearing data and setting back to original
-    function replayQuiz(){
-        highscoreContainer.style.display = "none";
-        endgameDiv.style.display = "none";
-        startQuizDiv.style.display = "flex";
-        timeLeft = 90;
-        score = 0;
-        currentQuestionIndex = 0;
-    }
-
-    //function checks the answer
-    function checkAnswer(answer){
-        correct = quizQuestions[currentQuestionIndex].correctAnswer;
-
-        if (answer === correct && currentQuestionIndex == finalQuestionIndex){
-            score++;
-            alert("Good Job");
-            currentQuestionIndex++;
-            generateQuizQuestion();
-            // shows if correct
-        }else if (answer == correct && currentQuestionIndex == finalQuestionIndex){
-            alert("You are Incorrect!")
-            currentQuestionIndex++;
-            generateQuizQuestion();
-            //shows if incorrect
-        }else{
-            showScore();
-        }
-    }
-
-    //start the quiz
-    startQuizButton.addEventListener("click",startQuiz);
+      } else {
+        confirmLower = confirm("Do you want lower case letters?");
+        console.log("Lower case " + confirmLower);
+        confirmUpper = confirm("Do you want upper case letters?");
+        console.log("Upper case " + confirmUpper);
+        confirmNumber = confirm("Do you want numbers?");
+        console.log("Number " + confirmNumber);
+        confirmSpecial = confirm("Do you want special characters?");
+        console.log("Special Character " + confirmSpecial);
     
-}
-]
-     
+    
+      };
+    
+    
+      // No answer then
+      if (!confirmLower && !confirmUpper && !confirmNumber && !confirmSpecial) {
+        userChoices = alert("You must choose a criteria");
+      // 4 true options
+      } else if (confirmLower && confirmUpper && confirmNumber && confirmSpecial) {
+        userChoices = lowerCase.concat(upperCase, numbers, special);
+        console.log(userChoices);
+      }
+      // 3 true options
+      else if (confirmLower && confirmUpper && confirmNumber) {
+        userChoices = lowerCase.concat(upperCase, numbers);
+        console.log(userChoices);
+      }
+      else if (confirmLower && confirmUpper && confirmSpecial) {
+        userChoices = lowerCase.concat(upperCase, special);
+        console.log(userChoices);
+      }
+      else if (confirmLower && confirmNumber && confirmSpecial) {
+        userChoices = lowerCase.concat(numbers, special);
+        console.log(userChoices);
+      }
+      else if (confirmUpper && confirmNumber && confirmSpecial) {
+        userChoices = upperCase.concat(numbers, special);
+        console.log(userChoices);
+      }
+      // 2 true options
+      else if (confirmLower && confirmUpper) {
+        userChoices = lowerCase.concat(upperCase);
+        console.log(userChoices);
+      }
+      else if (confirmLower && confirmNumber) {
+        userChoices = lowerCase.concat(numbers);
+        console.log(userChoices);
+      }
+      else if (confirmLower && confirmSpecial) {
+        userChoices = lowerCase.concat(special);
+        console.log(userChoices);
+      }
+      else if (confirmUpper && confirmNumber) {
+        userChoices = upperCase.concat(numbers);
+        console.log(userChoices);
+      }
+      else if (confirmUpper && confirmSpecial) {
+        userChoices = upperCase.concat(special);
+        console.log(userChoices);
+      }
+      else if (confirmNumber && confirmSpecial) {
+        userChoices = numbers.concat(special);
+        console.log(userChoices);
+      }
+      // 1 true option
+      else if (confirmLower) {
+        userChoices = lowerCase;
+        console.log(userChoices);
+      }
+      else if (confirmUpper) {
+        userChoices = upperCase.concat(upperCase);
+        console.log(userChoices);
+      }
+      else if (confirmNumber) {
+        userChoices = numbers;
+        console.log(userChoices);
+      }
+      else if (confirmSpecial) {
+        userChoices = special;
+        console.log(userChoices);
+      };
+    
+    
+      // Empty variable for the password lenght
+      var passwordBlank = [];
+      
+      // Loop for random selection
+      for (var i = 0; i < passwordLength; i++) {
+        var allChoices = userChoices[Math.floor(Math.random() * userChoices.length)];
+        passwordBlank.push(allChoices);
+        console.log(allChoices);
+      }
+    
+    
+      // Join and return the password 
+      var password = passwordBlank.join("");
+      console.log("Your Pasword is: " + password);
+      return password;
+      
+    }
+    
+      
+//not sure what I am missing. code looks good but when I bring up the page in a browser the button does not click. 
+// I showed a friend the code and he said it looks good, but we didn;t test it
